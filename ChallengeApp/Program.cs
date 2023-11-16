aa2a69c8ad704299c5e1bb2494a5873c8cddf580
KarolS <karol1214@o2.pl>
2023-11-16 21:32:43 +0000

    Tworzenie aplikacji ciag dalszy

---
+++ ChallengeApp/Program.cs
@@ -0,0 +1,17 @@
+﻿var name = "Ewa";
+var isWoman = true;
+var age = 33;
+
+
+if (name == "Ewa" && age == 33 && isWoman ==true)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (isWoman == true && age <30 )
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (isWoman == false && age <18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
